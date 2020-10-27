bbe05f970a280a2b9e9c2c9e6b6b083dc3c67b56
lunik1 <ch.gpg@themaw.xyz>
2020-10-27 15:12:13 +0000

    Update tree-sitter module

--- modules/tools/tree-sitter/config.el
+++ modules/tools/tree-sitter/config.el
@@ -1,5 +1,7 @@
 ;;; tools/tree-sitter/config.el -*- lexical-binding: t; -*-
 
+;; Does deferring loading to prog-mode-hook work ?
 (use-package! tree-sitter-langs)
+
 (global-tree-sitter-mode)
 (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
--- modules/tools/tree-sitter/packages.el
+++ modules/tools/tree-sitter/packages.el
@@ -1,14 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/tree-sitter/packages.el
 
-(package! tsc :recipe (:host github
-                       :repo "ubolonton/emacs-tree-sitter"
-                       :files ("core/*.el")))
-
-(package! tree-sitter :recipe (:host github
-                               :repo "ubolonton/emacs-tree-sitter"
-                               :files ("lisp/*.el")))
-
-(package! tree-sitter-langs :recipe (:host github
-                                     :repo "ubolonton/emacs-tree-sitter"
-                                     :files ("langs/*.el" "langs/queries")))
+(package! tree-sitter)
+(package! tree-sitter-langs)
