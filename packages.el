d3a192a5a67780f96e832172db8fe36f8992eb49
lunik1 <ch.gpg@themaw.xyz>
2020-10-27 15:36:26 +0000

    (Doom) Improve LSP in LaTeX

--- packages.el
+++ packages.el
@@ -52,6 +52,7 @@
 ;; (package! company-fuzzy)
 (package! flycheck-clojure)
 (package! info-colors)
+(package! lsp-latex)
 (package! magit-delta) ; https://tecosaur.github.io/emacs-config/config.html#magit
 (package! page-break-lines)
 (package! vdiff)
