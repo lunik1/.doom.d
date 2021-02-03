67a7e2e0c638a9d508dc2c23022de76cc68983e1
lunik1 <ch.gpg@themaw.xyz>
2021-02-03 18:23:58 +0000

    (Doom) Improve org-mode configuration

--- packages.el
+++ packages.el
@@ -57,6 +57,12 @@
 (package! lsp-julia :recipe (:host github :repo "non-jedi/lsp-julia"))
 (package! lsp-latex)
 (package! magit-delta) ; https://tecosaur.github.io/emacs-config/config.html#magit
+(package! org-appear :recipe (:host github :repo "awth13/org-appear"))
+(package! org-fragtog)
+(package! org-pandoc-import
+  :recipe (:host github
+           :repo "tecosaur/org-pandoc-import"
+           :files ("*.el" "filters" "preprocessors")))
 (package! org-ref)
 (package! org-roam-bibtex)
 (package! ox-gfm)
