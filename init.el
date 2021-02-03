575d120054759ce1b5be36905b1864f7d2db4990
lunik1 <ch.gpg@themaw.xyz>
2021-02-03 18:21:14 +0000

    (Doom) Disable unicode module
    Doom kept rebuilding the cache and unicode support seems to work fine
    without it

--- init.el
+++ init.el
@@ -44,7 +44,7 @@
        (popup +all)   ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
        treemacs          ; a project drawer, like neotree but cooler
-       unicode           ; extended unicode support for various languages
+       ; unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        (window-select +numbers +switch-window)     ; visually switch windows
