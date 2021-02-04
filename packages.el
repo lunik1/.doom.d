2b9fd3f0cad2e41ba734554827af497aee1adc8f
lunik1 <ch.gpg@themaw.xyz>
2021-02-04 15:29:20 +0000

    (Doom) Unpin rotate

--- packages.el
+++ packages.el
@@ -68,7 +68,7 @@
 (package! ox-gfm)
 (package! ox-rst)
 (package! page-break-lines)
-(package! rotate :pin "091b5ac4fc310773253efb317e3dbe8e46959ba6") ; cl-lib version is broken
+(package! rotate)
 (package! vdiff)
 (package! vimrc-mode)
 ;; (package! visual-fill-column) ; https://github.com/emacs-evil/evil/issues/188
