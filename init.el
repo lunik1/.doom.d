b8f3f5ab419ccabc3cda559b2fb10ef52bd31a88
lunik1 <ch.gpg@themaw.xyz>
2023-04-27 01:01:50 +0100

    disable ace-window

--- init.el
+++ init.el
@@ -46,7 +46,7 @@
        ;; unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
-       (window-select +numbers +ace-window)     ; visually switch windows
+       (window-select +numbers)     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
        zen               ; distraction-free coding or writing (annd mixed-pitch)
 
