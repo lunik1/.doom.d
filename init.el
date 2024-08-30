d390151f2b94c53383e119f0cca314a60d1b8428
lunik1 <ch.gpg@themaw.xyz>
2024-08-31 00:38:16 +0100

    remove deprecated hydra and rgb modules

--- init.el
+++ init.el
@@ -33,7 +33,6 @@
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
-       hydra
        indent-guides     ; highlighted indent columns
        (ligatures +extra)         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
@@ -100,7 +99,6 @@
        ;;pass              ; password manager for nerds
        (:if (not IS-WINDOWS) pdf)               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
-       rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
