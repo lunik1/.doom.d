258d38c16d730d9536b14bec7094c02cfc08efea
lunik1 <ch.gpg@themaw.xyz>
2026-03-13 21:39:39 +0000

    use setf to set doom options

--- config.el
+++ config.el
@@ -79,16 +79,16 @@
                "\n"))
      'face 'doom-dashboard-banner)))
 
-(setopt +doom-dashboard-banner-dir "~/.doom.d/"
-        +doom-dashboard-banner-file "tem.png"
-        +doom-dashboard-ascii-banner-fn #'tem-dashboard-draw-ascii-banner-fn
-        doom-font (font-spec :family "Myosevka" :size 13.0)
-        doom-symbol-font (font-spec :family "Julia Mono")
-        doom-variable-pitch-font (font-spec :family "Myosevka Etoile" :size 13.0)
-        doom-serif-font (font-spec :family "Myosevka Etoile" :size 13.0)
-        doom-theme 'doom-gruvbox
-        doom-themes-enable-bold t
-        doom-themes-enable-italic t)
+(setf +doom-dashboard-banner-dir "~/.doom.d/"
+      +doom-dashboard-banner-file "tem.png"
+      +doom-dashboard-ascii-banner-fn #'tem-dashboard-draw-ascii-banner-fn
+      doom-font (font-spec :family "Myosevka" :size 13.0)
+      doom-symbol-font (font-spec :family "Julia Mono")
+      doom-variable-pitch-font (font-spec :family "Myosevka Etoile" :size 13.0)
+      doom-serif-font (font-spec :family "Myosevka Etoile" :size 13.0)
+      doom-theme 'doom-gruvbox
+      doom-themes-enable-bold t
+      doom-themes-enable-italic t)
 
 ;; theme tweaks
 (custom-set-faces!
